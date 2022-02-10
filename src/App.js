dbced121bc6aed1ba3bd5aee438d2a07751ddf41
abidwai <abidwai@cemtrexlabs.com>
2022-02-10 20:06:12 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <div className="App" style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
         <Button type="button" label="toggle button" onClick={toggleTheme}>toggle button</Button>
         <h1>Heading 1</h1>
+        <Button type="button" label="dummy">dummy</Button>
       </div>
     </ThemeContext.Provider>
   );
--- src/components/Elements.js
+++ src/components/Elements.js
@@ -1,15 +1,14 @@
-import { useContext } from "react";
 import { ThemeContext } from "../context/theme_context";
 
 export function Button(props) {
     const { label, ...rest } = props;
-    const theme = useContext(ThemeContext);
-    let style = {};
-    if (theme) {
-        style = {
-            backgroundColor: theme.backgroundColor,
-            color: theme.color
-        }
-    }
-    return <button area-label={label} {...rest} >{props.children && props.children}</button>
+    return (
+        <ThemeContext.Consumer>
+            {
+                (theme) => (
+                    <button area-label={label} {...rest} style={{ ...theme }} >{props.children && props.children}</button>
+                )
+            }
+        </ThemeContext.Consumer>
+    )
 }
