39d19e1dea930621557ad7ac828b7670416f7d01
abidwai <bidwaiav@gmail.com>
2022-07-01 15:36:16 +0530

    react redux practice

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,14 +1,17 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './actionTypes';
 
-export function increment_counter(incrementBy) {
+function increment_counter(incrementBy) {
     return {
         type: INCREMENT_COUNTER,
         payload: incrementBy
     }
 };
 
-export function decrement_counter() {
+function decrement_counter() {
     return {
         type: DECREMENT_COUNTER
     }
 }
+
+// this is like named export
+export { increment_counter, decrement_counter }
