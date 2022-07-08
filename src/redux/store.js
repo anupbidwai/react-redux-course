3bdcb3bf5257f622863e505f05e12909ec7d8db3
Anup Bidwai <bidwaiav@gmail.com>
2022-07-08 12:18:44 +0530

    practice on redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slice/counterSlice';
 import headingReducer from './slice/headingSlice';
-
 export default configureStore({
     reducer: {
         counter: counterReducer,
